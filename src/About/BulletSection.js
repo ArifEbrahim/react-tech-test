9b2e1a4015896d4eb0e5ad36f55a1f0ec945191d
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-09-15 20:06:53 +0100

    small CSS tweaks

--- src/About/BulletSection.js
+++ src/About/BulletSection.js
@@ -3,14 +3,13 @@ import React from 'react'
 export default function BulletSection() {
   return (
     <div>
-      <h5>Taria duo ut vis semper abhorreant:</h5>
+      <div class='bullet-title'>Taria duo ut vis semper abhorreant:
+      </div>
       <ul>
-        <strong>
           <li>Te pri efficiendi assueverit, id molestie suavitate per</li>
           <li>Te nam dolorem rationibus repudiandae, ne ius falli aliquip consetetur</li>
           <li>Ut qui dicant copiosae interpretaris</li>
           <li>Ut indoctum patrique coluptaria duo, ut vis semper abhorreant</li>
-        </strong>
       </ul>
     </div>
   )
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -5,16 +5,20 @@ import Logo from '../assets/Logo.svg'
 
 export default function Navbar() {
   return (
-    <nav>
-      <div className='nav-img'>
-        <img src={Logo} alt='logo' className='logo'/>
+    <header>
+      <div className='container'>
+        <nav>
+          <div className='nav-img'>
+            <img src={Logo} alt='logo' className='logo'/>
+          </div>
+          <div className='nav-list'>
+            <Link to="/">Home</Link>
+            <Link to="/about-us">About Us</Link>
+            <Link to="/contact-us">Contact Us</Link>
+            <button className='login-btn'>Log in</button>
+          </div>
+        </nav>
       </div>
-      <div className='nav-list'>
-        <Link to="/">Home</Link>
-        <Link to="/about-us">About Us</Link>
-        <Link to="/contact-us">Contact Us</Link>
-        <button className='login-btn'><strong>Log in</strong></button>
-      </div>
-    </nav>
+    </header>
   )
 }
