8ae79a923219ab3b8334df24ac8b423b818935c0
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-09-15 09:27:16 +0100

    router setup and different views created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import About from './About/About'
+import Contact from './Contact/Contact'
+import Home from './Home/Home'
+
 
 function App() {
   return (
-    <div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about-us">About Us</Link>
+            </li>
+            <li>
+              <Link to="/contact-us">Contact Us</Link>
+            </li>
+          </ul>
+        </nav>
 
-    </div>
+        <Switch>
+          <Route path="/about-us">
+            <About />
+          </Route>
+          <Route path="/contact-us">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
