d279b6a78dcbce929f2f32a2c42c99e47d832036
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-09-15 17:31:09 +0100

    Navbar complete

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import './Navbar.css'
+import Logo from '../assets/Logo.svg'
 
 export default function Navbar() {
   return (
     <nav>
+      <div className='nav-img'>
+        <img src={Logo} alt='logo' className='logo'/>
+      </div>
       <div className='nav-list'>
         <Link to="/">Home</Link>
         <Link to="/about-us">About Us</Link>
