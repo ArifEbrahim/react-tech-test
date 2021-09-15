963f66373ebaf2a6c216686922c448023839e4d5
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-09-15 11:43:07 +0100

    Added sample text to about page

--- src/About/About.js
+++ src/About/About.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { topLine, paragraphOne, paragraphTwo, paragraphThree, paragraphFour, paragraphFive } from './sampleText'
+import OfficeImage from '../assets/shutterstock_696636415.jpg'
+import BulletSection from './BulletSection'
 
 export default function About() {
+
   return (
     <div>
-      <h1>About</h1>
+      <h3>About Us</h3>
+      {topLine}
+      <p>{paragraphOne}</p>
+      <p>{paragraphTwo}</p>
+      <img src={OfficeImage} alt='office'/>
+      <p>{paragraphThree}</p>
+      <BulletSection />
+      <p>{paragraphFour}</p>
+      <p>{paragraphFive}</p>
     </div>
   )
 }
