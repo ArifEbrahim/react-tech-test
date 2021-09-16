c9e8551f24a3cfb596cc252001e89ca43192974c
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-09-16 12:29:46 +0100

    Homepage section 1 complete

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import './Home.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='homepage-container'>
+      <div className='homepage-section1'>
+        <div className='section1-box1'>
+          <div className='box-title'>Lorem ipsum dolor</div>
+          <p>Quem vide tincidunt pri ei, id mea omnium denique.</p>
+          <button className='contact-btn'>Contact us</button>
+        </div>
+      </div>
     </div>
   )
 }
