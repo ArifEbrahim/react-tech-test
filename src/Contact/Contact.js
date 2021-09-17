df7ad6b4f92dece0f7ded054d233b19304b37bb7
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-09-17 13:31:36 +0100

    Contact us background image and name-email complete

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import './Contact.css'
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className='contact-us-container'>
+      <div className='background-img'>
+        <div className='form-container'>
+          <div className='section-title'>Contact us</div>
+          <p>Populo facilisi nam no, dolor deleniti deseruisse ne cum, nam quodsi aliquam eligendi ne. Ferri euismod accusata te nec, summo accumsan at vix.</p>
+          <form>
+            <div className='name-email-box'>
+              <div className='name-email'>
+                <label>Full name</label>
+                <input type='text'></input>
+              </div>
+              <div className='name-email'>
+                <label>Email address</label>
+                <input type='email'></input>
+              </div>
+            </div>
+            
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
