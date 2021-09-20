3d6594b243e1ce93646c34cc262189b183c5714c
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-09-20 14:59:34 +0100

    Contact Us page now displays errors

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -10,6 +10,13 @@ export default function Contact() {
   const [phone01, setPhone01] = useState("")
   const [phone02, setPhone02] = useState("")
   const [message, setMessage] = useState("")
+  const [nameError, setNameError] = useState("")
+  const [emailError, setEmailError] = useState("")
+  const [phone01Error, setPhone01Error] = useState("")
+  const [phone02Error, setPhone02Error] = useState("")
+  const [messageError, setMessageError] = useState("")
+
+
   let phoneNumbers = [];
   const apiURL='https://interview-assessment.api.avamae.co.uk/api/v1/contact-us/submit'
 
@@ -39,7 +46,8 @@ export default function Contact() {
       }
     })
     .catch(error => {
-      console.log(error.response.data)
+      console.log(error.response.data.Errors)
+      handleErrors(error.response.data.Errors)
     })
   }
 
@@ -55,6 +63,29 @@ export default function Contact() {
     successMsgEl.style.display = 'flex'
   }
 
+  const handleErrors =(errors) => {
+    errors.forEach(error => {
+      switch(error.FieldName) {
+        case 'FullName':
+          setNameError(error.MessageCode)
+          break;
+        case 'EmailAddress':
+          setEmailError(error.MessageCode)
+          break;
+        case 'PhoneNumbers[0]':
+          setPhone01Error(error.MessageCode.replaceAll('_', ' '))
+          break;
+        case 'PhoneNumbers[1]':
+          setPhone02Error(error.MessageCode.replaceAll('_', ' '))
+          break;
+        case 'Message':
+          setMessageError(error.MessageCode)
+          break;
+        default:
+      }
+    })
+  }
+
   return (
     <div className='contact-us-container'>
       <div className='background-img'>
@@ -67,19 +98,23 @@ export default function Contact() {
               <div className='name-email'>
                 <label>Full name</label>
                 <input type='text' value={fullName} onChange={(e) => {setFullName(e.target.value)}}></input>
+                <div className='error-msg'>{nameError}</div>
               </div>
               <div className='name-email'>
                 <label>Email address</label>
                 <input type='email' value={email} onChange={(e) => {setEmail(e.target.value)}}></input>
+                <div className='error-msg'>{emailError}</div>
               </div>
             </div>
             <div className='phone-no' id='phone01'>
               <label>Phone number 01<span>-optional</span></label>
               <input type='text' value={phone01} onChange={(e) => {setPhone01(e.target.value)}}></input>
+              <div className='error-msg'>{phone01Error}</div>
             </div>
             <div className='phone-no' id='phone02'>
               <label>Phone number 02<span>-optional</span></label>
               <input type='text' value={phone02} onChange={(e) => {setPhone02(e.target.value)}}></input>
+              <div className='error-msg'>{phone02Error}</div>
             </div>
               <Button size={'xl'} colour={'light'} text={'Add new phone number'} onClick={showPhone02}/>
             <div className='message-title-box'>
@@ -87,7 +122,10 @@ export default function Contact() {
               <span>Maximum text length is 500 characters</span>
             </div>
             <textarea value={message} onChange={(e) => {setMessage(e.target.value)}}></textarea>
-            <Button size={'xl'} colour={'blue'} text={'Submit'} onClick={handleSubmit}/>
+            <div className='error-msg'>{messageError}</div>
+            <div className='submit-box'>
+              <Button size={'xl'} colour={'blue'} text={'Submit'} onClick={handleSubmit}/>
+            </div>
           </form>
         </div>
       </div>
